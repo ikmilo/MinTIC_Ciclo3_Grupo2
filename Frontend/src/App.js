8f816fc30bf9d6bf4516d87422c51ffa9515126a
christian.acevedo <christian.acevedo@softtek.com>
2022-09-09 20:06:35 -0300

    Primera Aplicación

---
+++ Frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import PrimarySearchAppBar from './components/PrimarySearchAppBar'
+import MultiActionAreaCard from './components/MultiActionAreaCard'
+import SpanningTable from './components/SpanningTable'
+import HorizontalNonLinearStepper from './components/MultiActionAreaCard'
+
+const App = () => {
+  return (
+    <div className="App">
+      <PrimarySearchAppBar></PrimarySearchAppBar>
+      
+    </div>
+  );
+}
+
+export default App;
