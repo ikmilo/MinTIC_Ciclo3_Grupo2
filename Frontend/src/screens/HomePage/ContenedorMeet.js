17981ea70dd09503133b1a9b2f32236c3d6c1001
christian.acevedo <christian.acevedo@softtek.com>
2022-09-15 21:11:18 -0300

    Contenedor hecho en la meet

---
+++ Frontend/src/screens/HomePage/ContenedorMeet.js
@@ -0,0 +1,22 @@
+import SpacingGrid from '../../components/SpacingGrid'
+
+const ContenedorMeet = () => {
+
+    const containerStyles = {
+        margin:'auto',
+        marginTop:'3%',
+        //backgroundColor: 'red',
+        width:'80%',
+        //height:'20em',
+    }
+
+    return (
+        <div style={containerStyles}>
+            <SpacingGrid></SpacingGrid>
+        </div>
+    )
+};
+
+export default ContenedorMeet
+
+
