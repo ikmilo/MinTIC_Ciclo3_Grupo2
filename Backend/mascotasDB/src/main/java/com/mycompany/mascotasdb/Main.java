6390b32de049e3479b93915dff030fb7d3a49804
jacen20 <jacen.sanchez@outlook.com>
2022-09-27 20:51:18 -0500

    se agrega la libreria de mysql

---
+++ Backend/mascotasDB/src/main/java/com/mycompany/mascotasdb/Main.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.mascotasdb;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+//import com.google.gson.Gson;
+import connection.DBConnection;
+
+/**
+ *
+ * @author Jacen
+ */
+public class Main {
+ 
+    DBConnection con = new DBConnection();
+    
+}
